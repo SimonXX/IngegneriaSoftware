dc2af3f92a46f1698531480ce0acf000bcea008d
Simon <100418694+SimonXX@users.noreply.github.com>
2023-05-17 15:46:06 +0200

    update

--- src/designPatterns/Command/struttura/Invoker.java
+++ src/designPatterns/Command/struttura/Invoker.java
@@ -3,7 +3,7 @@ package designPatterns.Command.struttura;
 public class Invoker {
     private Command command;
 
-    public void handle(Command cmd) {
+    public void handle(Command cmd) {//possiamo passare anche un MacroCommand
         this.command = cmd;
         this.command.execute();
     }
--- src/designPatterns/Visitor/ElementA.java
+++ src/designPatterns/Visitor/ElementA.java
@@ -3,6 +3,8 @@ package designPatterns.Visitor;
 public class ElementA implements Element{
     @Override
     public void accept(Visitor visitor) {//ElementA invoca il metodo fornendo se stesso come parametro
+        //double dispace -> il comportamento del metodo non dipende solo
+        //dall'oggetto su cui invochiamo (visitor) ma anche da quello che ha invocato (elementA) -> infatti lo passiamo come parametro
         visitor.visitElementA(this);
     }
 
